b74e13550bd82f8e91388787f7a9cdc249fbbe09
yago.juan <yago.juan@teclat.com.br>
2022-10-24 09:03:43 -0300

    feat: create expo-updates function (OTA)

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NativeBaseProvider } from "native-base";
 import { QueryClientProvider } from "react-query";
+import * as Updates from "expo-updates";
 
 import { AppProvider } from "./src/contexts";
 
@@ -7,8 +8,20 @@ import { queryClient } from "./src/services/queryClient";
 
 import { Routes } from "./src/routes/index.routes";
 import { NavigationContainer } from "@react-navigation/native";
+import { useEffect } from "react";
 
 function App() {
+  async function updateApp() {
+    const { isAvailable } = await Updates.checkForUpdateAsync();
+
+    if (isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  }
+
+  useEffect(() => updateApp, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
